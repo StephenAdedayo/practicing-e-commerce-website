77cabf4c086b76842a6e7d2c25fc8e77e8337cfc
StephenAdedayo <alustephenadedayo@gmail.com>
2024-05-30 10:47:59 -0700

    testing responsiveness

---
+++ index.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('ri-menu-line');
+    navbar.classList.toggle(open);
+};
+
+window.onscroll = () => {
+    menu.classList.remove('ri-menu-line');
+    navbar.classList.remove(open);
+};
